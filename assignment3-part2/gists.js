fa389a32a5dcd48526e6912ec87b33f4fca427f5
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-04-26 11:41:22 -0500

    Fixed findById, tracking down token o error

--- assignment3-part2/gists.js
+++ assignment3-part2/gists.js
@@ -29,11 +29,73 @@ function searchMe() {
 
 function loadGists() {
 	var parsed = JSON.parse(localStorage.getItem('gistList'));
-	console.log(parsed);
-	
+	// to store html coded gists
+	var gistSide = document.getElementById('gists');
+	var favSide = document.getElementById('faves');
+
+	for (var i = 0; i < parsed.length; i++) {
+		var output = makeHTML(parsed[i]);
+
+		gistSide.appendChild(output);
+	}
+
+	var faveList = JSON.parse(localStorage.getItem('favList'));
+
+	for (var j = 0; j < faveList.length; j++) {
+		var posted = makeHTML(faveList[j]);
+
+		favSide.appendChild(posted);
+	}
 }
 
-function moveToFavs(gistObject) {
-	var fave = document.getElementById('faves');
+function makeHTML(gistObject) {
+
+	var desc = document.createElement('div');
+	desc.setAttribute('id', 'desc');
+
+	if (gistObject.description === null) {
+		desc.innerHTML = "No description!";
+	}
+	else {
+		desc.innerHTML = gistObject.description;
+	}
 	
+	var url = document.createElement('div');
+	url.innerHTML = '<a href="' +gistObject.url + '">' + gistObject.url + '</a>';
+
+	var id = document.createElement('div');
+	id.setAttribute('id', gistObject.id);
+
+	var fbutton = document.createElement('button');
+	fbutton.innerHTML = "+";
+	fbutton.setAttribute("gistId", gistObject.id);
+
+	fbutton.onclick = function() {
+		var gistId = this.getAttribute("gistId"); //this is what you have saved before
+		var toBeFavoredGist = findById(gistId);
+		localStorage.setItem('favList', toBeFavoredGist);
+		localStorage.removeItem('gistList', toBeFavoredGist);
+	//here you add the gist to your favorite list in the localStorage and remove it from the gist list and add it to favorite list
+	};
+
+	var htmlGist = document.createElement('div');
+	var spacer = document.createElement('p');
+
+	htmlGist.appendChild(fbutton);
+	htmlGist.appendChild(desc);
+	htmlGist.appendChild(url);
+	htmlGist.appendChild(id);
+	htmlGist.appendChild(spacer);
+
+	return htmlGist;
+}
+
+function findById(gistId) {
+	var finding = JSON.parse(localStorage.getItem('gistList'));
+
+	for (var i = 0; i < finding.length; i++) {
+		if (finding[i].id === gistId) {
+			return finding[i];
+		}
+	}
 }
