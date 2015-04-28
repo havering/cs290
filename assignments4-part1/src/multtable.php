837f832125b435f553a5dabd2a486b8a77687b7a
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-04-28 09:54:36 -0500

    Started multtable, error handling done

--- assignments4-part1/src/multtable.php
+++ assignments4-part1/src/multtable.php
@@ -1,22 +1,45 @@
 <?php
-	error_reporting(E_ALL);
-	init_set('display_errors', 'On');
-	?>
-<!DOCTYPE html>
-<html>
+echo '<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>Assignment 4</title>
+<meta charset="utf-8" />
+<title></title>
 </head>
-<body>
+<body>';
 
-<?php 
-	//create form for input
-	echo '<form><p>Enter the minimum multiplicand: <input type="number" id="minCand">';
-	echo '<br>Enter the maximum multiplicand: <input type="number" id="maxCand">';
-	echo '<br>Enter the minimum multiplier: <input type="number" id="minMult">';
-	echo '<br>Enter the maximum multiplier: <input type="number" id="maxMult">';
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 
+// input validation
+if ($_GET['minCand'] > $_GET['maxCand']) {
+	echo '<p>Minimum multiplicand larger than maximum.';
+}
+if ($_GET['minPlier'] > $_GET['maxPlier']) {
+	echo '<p>Minimum multiplier larger than maximum.';
+}
+
+foreach($_GET as $key => $value) {
+	if (!(is_numeric($value))) {
+		echo '<p>' . $key . ' must be an integer';
+	}
+}
+
+foreach($_GET as $key => $value) {
+	if (empty($value)) {
+		echo '<p>Missing parameter ' . $key . ".";
+	}
+}
+
+// table creation
+echo '<p>Multiplication table
+<table border="1">
+<tr>
+<td>Key
+<td>Value';
+foreach($_GET as $key => $value) {
+	echo '<tr><td>' . $key . '<td>' . $value;
+}
+echo '</table>';
+	echo '</body>
+</html>';
 ?>
-
-</body>
-</html>
