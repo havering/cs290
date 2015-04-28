badbb8518a0354524de01ae4ce521cbf9262cdea
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-04-27 22:31:55 -0500

    Assignment 4 - started, this doesn't work at all

---
+++ assignments4-part1/src/multtable.php
@@ -0,0 +1,22 @@
+<?php
+	error_reporting(E_ALL);
+	init_set('display_errors', 'On');
+	?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assignment 4</title>
+</head>
+<body>
+
+<?php 
+	//create form for input
+	echo '<form><p>Enter the minimum multiplicand: <input type="number" id="minCand">';
+	echo '<br>Enter the maximum multiplicand: <input type="number" id="maxCand">';
+	echo '<br>Enter the minimum multiplier: <input type="number" id="minMult">';
+	echo '<br>Enter the maximum multiplier: <input type="number" id="maxMult">';
+
+?>
+
+</body>
+</html>
