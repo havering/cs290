cee14fd7a9da5dca9239789e7e50de72563e6b21
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-04-29 19:41:41 -0500

    Started login.php - form creation

---
+++ assignments4-part1/src/login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+	<form action="content1.php" method="POST">
+		<p>Enter a username: <input type="text">
+		<p><input type="submit" value="Submit">
+	</form>
