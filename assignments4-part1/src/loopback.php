d963695b5bef28ade953f2a851372b1ae0daca83
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-05-02 20:43:19 -0500

    Typo in loopback ruining everything, fixed and done

--- assignments4-part1/src/loopback.php
+++ assignments4-part1/src/loopback.php
@@ -1,27 +1,26 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors',1);
-header('Content-type: text/plain');
+header('Content-type: text/html');
 
+//$output = "";
 
-
-if ($_SERVER['REQUEST_METHOD'] == $_POST) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 	if (empty($_POST)) {
-		$_POST['parameters'] = null;
+		$_POST["parameters"] = null;
 	}
-	$_POST['Type'] = 'POST';
-	//$poster = json_encode($_POST);
-	echo json_encode($_POST);
-}
-else if ($_SERVER['REQUEST_METHOD'] == $_GET) {
+	$_POST["Type"] = "POST";
+	$output = json_encode($_POST);
+} 
+else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	if (empty($_GET)) {
-		$_GET['parameters'] = null;
+		$_GET["parameters"] = null;
 	}
-	$_GET['Type'] = 'GET';
-	//$getter = json_encode($_GET);
-	echo json_encode($_GET);
+	$_GET["Type"] = "GET";
+	$output = json_encode($_GET);
 }
-echo json_encode($_GET);
-echo json_encode($_POST);
+
+echo $output;
+
 
 ?>
