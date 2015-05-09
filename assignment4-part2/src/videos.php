7ea3d7680cc6a14f251b82ead3d2c92ba8e5b741
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-05-09 11:23:37 -0500

    Successful connection to DB

--- assignment4-part2/src/videos.php
+++ assignment4-part2/src/videos.php
@@ -5,7 +5,20 @@
 </head>
 <body>
 	<?php
+		$host = 'oniddb.cws.oregonstate.edu';
+		$db = 'ohaverd-db';
+		$user = 'ohaverd-db';
+		$pw = 'password removed for github push'
 
+		$mysqli = new mysqli($host, $user, $pw, $db);
+		if ($mysqli->connect_errno) {
+			echo 'Failed to connect to MySQLi: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
+		}
+		else {
+			echo 'Connection successful';
+		}
 	?>
+
+
 </body>
 </html>
