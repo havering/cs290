d394f3e4010e0000031a8c339d9cf6aadaff4d8e
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-05-09 14:03:49 -0500

    Some form validation added + prepared statement

--- assignment4-part2/src/videos.php
+++ assignment4-part2/src/videos.php
@@ -1,31 +1,58 @@
+<?php
+
+	$host = 'oniddb.cws.oregonstate.edu';
+	$db = 'ohaverd-db';
+	$user = 'ohaverd-db';
+	$pw = 'removed for git push';
+
+	$mysqli = new mysqli($host, $user, $pw, $db);
+	if ($mysqli->connect_errno) {
+		echo 'Failed to connect to MySQLi: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
+	}
+
+	if(isset($_GET)) {
+		$reachEnd = true;
+
+		// error handling from form
+		foreach($_GET as $key => $value) {
+			if (empty($value)) {
+				echo '<p>Missing parameter ' . $key . ".";
+				$reachEnd = false;
+			}
+		}
+
+		if ($reachEnd) {
+			$vid = $_GET['vidName'];
+			$cat = $_GET['vidCat'];
+			$len = $_GET['vidLen'];
+
+			if (!($statement = $mysqli->prepare("INSERT INTO videos (name, category, length) VALUES ('$vid', '$cat', '$len')"))) {
+				echo 'Prepare statement failed.';
+			}
+
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Video Store</title>
 </head>
 <body>
-	<?php
-		$host = 'oniddb.cws.oregonstate.edu';
-		$db = 'ohaverd-db';
-		$user = 'ohaverd-db';
-		$pw = 'deleted for github push';
-
-		$mysqli = new mysqli($host, $user, $pw, $db);
-		if ($mysqli->connect_errno) {
-			echo 'Failed to connect to MySQLi: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
-		}
-		else {
-			echo 'Connection successful';
-		}
-	?>
-
-<div style="font-family: sans serif">
+	
+<div name="form" method="GET" action="videos.php">
 	<h3>Welcome to the Video Store</h3>
 	<form name="addVid">
 		<p>Name: <input type="text" name="vidName">
 		<p>Category: <input type="text" name="vidCat">
 		<p>Length: <input type="number" min="0" name="vidLen">
 		<p><input type="submit" value="Add movie">
+		</form>
 
+<!--table display-->
+<?php
+
+?>
 </body>
 </html>
