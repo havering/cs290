0408666a15096bf3b49bac9fa94ba4239fc51c82
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-05-09 15:49:28 -0500

    Prepare and execute working

--- assignment4-part2/src/videos.php
+++ assignment4-part2/src/videos.php
@@ -3,14 +3,14 @@
 	$host = 'oniddb.cws.oregonstate.edu';
 	$db = 'ohaverd-db';
 	$user = 'ohaverd-db';
-	$pw = 'removed for git push';
+	$pw = 'deleted for git push';
 
 	$mysqli = new mysqli($host, $user, $pw, $db);
 	if ($mysqli->connect_errno) {
 		echo 'Failed to connect to MySQLi: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
 	}
 
-	if(isset($_GET)) {
+	if(isset($_GET['vidName'])) {
 		$reachEnd = true;
 
 		// error handling from form
@@ -22,14 +22,18 @@
 		}
 
 		if ($reachEnd) {
+			
+			$stmt = $mysqli->prepare("INSERT INTO videos (name, category, length, rented) VALUES (?, ?, ?, ?)");
+			$stmt->bind_param('ssii', $vid, $cat, $len, $rented);
+
 			$vid = $_GET['vidName'];
 			$cat = $_GET['vidCat'];
 			$len = $_GET['vidLen'];
+			$rented = 0;	// rented is false as it is just being added
 
-			if (!($statement = $mysqli->prepare("INSERT INTO videos (name, category, length) VALUES ('$vid', '$cat', '$len')"))) {
-				echo 'Prepare statement failed.';
-			}
-
+			$stmt->execute();
+			
+			$stmt->close();
 		}
 	}
 ?>
@@ -46,7 +50,7 @@
 	<form name="addVid">
 		<p>Name: <input type="text" name="vidName">
 		<p>Category: <input type="text" name="vidCat">
-		<p>Length: <input type="number" min="0" name="vidLen">
+		<p>Length: <input type="number" min="1" name="vidLen">
 		<p><input type="submit" value="Add movie">
 		</form>
 
